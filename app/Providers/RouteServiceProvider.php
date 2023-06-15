fbef4f42a75dc6ec528f9d91159143c7a9482096
Ehab Elsese <ehabpop20178@gmail.com>
2023-06-15 12:59:45 +0300

    prepare dashboard

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,6 +35,12 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('site')
+                ->group(base_path('routes/site.php'));
+
+            Route::middleware('admin')
+                ->group(base_path('routes/admin.php'));
         });
     }
 }

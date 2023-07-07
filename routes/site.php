97f635fb01b54956c3bb05e98050d60b705836c9
Ehab El-Sisi <ehab.el-sisi@kit-eg.com>
2023-07-07 17:18:55 +0200

    frontEnd Them Startup

--- routes/site.php
+++ routes/site.php
@@ -8,8 +8,62 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
+
+
+
+
+    Route::group(['namespace' => 'Site'/*, 'middleware' => 'guest'*/], function () {
+        //guest  user
+Route::get('fat','PaymentController@fatoorah');
+        route::get('/', 'HomeController@home')->name('home')->middleware('VerifiedUser');
+        route::get('category/{slug}', 'CategoryController@productsBySlug')->name('category');
+        route::get('product/{slug}', 'ProductController@productsBySlug')->name('product.details');
+
+        /**
+         *  Cart routes
+         */
+        Route::group(['prefix' => 'cart'], function () {
+            Route::get('/', 'CartController@getIndex')->name('site.cart.index');
+            Route::post('/cart/add/{slug?}', 'CartController@postAdd')->name('site.cart.add');
+            Route::post('/update/{slug}', 'CartController@postUpdate')->name('site.cart.update');
+            Route::post('/update-all', 'CartController@postUpdateAll')->name('site.cart.update-all');
+        });
+    });
+
+
+    Route::group(['namespace' => 'Site', 'middleware' => ['auth', 'VerifiedUser']], function () {
+        // must be authenticated user and verified
+        Route::get('profile', function () {
+            return 'You Are Authenticated ';
+        });
+    });
+
+    Route::group(['namespace' => 'Site', 'middleware' => 'auth'], function () {
+        // must be authenticated user
+        Route::post('verify-user/', 'VerificationCodeController@verify')->name('verify-user');
+        Route::get('verify', 'VerificationCodeController@getVerifyPage')->name('get.verification.form');
+        Route::get('products/{productId}/reviews', 'ProductReviewController@index')->name('products.reviews.index');
+        Route::post('products/{productId}/reviews', 'ProductReviewController@store')->name('products.reviews.store');
+        Route::get('payment/{amount}', 'PaymentController@getPayments') -> name('payment');
+        Route::post('payment', 'PaymentController@processPayment') -> name('payment.process');
+
+    });
+
+
+
+Route::group(['namespace' => 'Site', 'middleware' => 'auth'], function () {
+    Route::post('wishlist', 'WishlistController@store')->name('wishlist.store');
+    Route::delete('wishlist', 'WishlistController@destroy')->name('wishlist.destroy');
+    Route::get('wishlist/products', 'WishlistController@index')->name('wishlist.products.index');
+});
+
+
+Route::get('front',function (){
+
+    return view('front.home');
+});
